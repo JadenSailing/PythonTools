62e8baa0a48ac7588ec56d2037f8113d7858bcce
jadensailing <jadensailing@126.com>
2021-05-28 16:52:46 +0800

    Update ToolsEntry.py

--- ToolsEntry.py
+++ ToolsEntry.py
@@ -9,12 +9,13 @@ from Tools import *
 cfg = configparser.ConfigParser()
 cfg.read("Config.ini", encoding = "utf8")
 
-print(cfg.get("Global", "Comment"))
+print("# " + cfg.get("Global", "Comment"))
 toolId = cfg.getint("Global", "Use")
-print("now use id = %d" % (toolId))
+print("# now use tool %d" % (toolId))
 toolSection = ("Tool%d" % toolId)
 comment = cfg.get(toolSection, "Comment")
-print(comment + "...")
+print("# " + comment + "...")
+print(">>>")
 if(toolId == 1):
     counter = CodeLineCounter()
     dir = cfg.get(toolSection, "Dir")
@@ -23,6 +24,8 @@ if(toolId == 1):
     filter = cfg.get(toolSection, "Filter")
     counter.SetFilter(filter)
     dirCount, fileCount, lineCount = counter.Count()
-    print("dirCount = %d, fileCount = %d, lineCount = %d" % (dirCount, fileCount, lineCount))
+    print("dirCount = %d" % (dirCount, ))
+    print("fileCount = %d" % (fileCount, ))
+    print("lineCount = %d" % (lineCount,))
     pass
 pass
